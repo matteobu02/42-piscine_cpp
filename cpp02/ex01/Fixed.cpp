c8098d675c094638802635e14d5ae0dc8c09aebf
mbucci <mbucci@student.s19.be>
2022-04-05 00:29:13 +0200

    ex01 done

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/03 21:05:13 by mbucci            #+#    #+#             */
+/*   Updated: 2022/04/05 00:24:13 by mbucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <fstream>
+
+//////////////////
+// Constructors //
+//////////////////
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_RawBits = 0;
+	return ;
+}
+
+Fixed::Fixed(int x)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_RawBits = x << Fixed::_NbBits;
+	return ;
+}
+
+Fixed::Fixed(float const x)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_RawBits = x * (1 << Fixed::_NbBits);
+	return ;
+}
+
+Fixed::Fixed(Fixed const & obj)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->_RawBits = obj.getRawbits();
+	return ;
+}
+
+/////////////////
+// Destructors //
+/////////////////
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+//////////////////////////
+// Overloaded operators //
+//////////////////////////
+
+Fixed	& Fixed::operator= (Fixed const & rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_RawBits = rhs.getRawbits();
+	return (*this);
+}
+
+///////////////////////
+// Memeber functions //
+///////////////////////
+
+int		Fixed::getRawbits(void) const
+{
+	return (this->_RawBits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)(this->_RawBits) / (1 << Fixed::_NbBits));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_RawBits >> Fixed::_NbBits);
+}
+
+std::ostream	& operator<< (std::ostream & ofs, Fixed const & fixed)
+{
+	ofs << fixed.toFloat();
+	return (ofs);
+}
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,27 +6,40 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 01:42:07 by mbucci            #+#    #+#             */
-/*   Updated: 2022/04/03 12:45:16 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/04/05 00:19:43 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <fstream>
+
 class Fixed
 {
 	public:
+		// Constructors
 		Fixed(void);
+		Fixed(int x);
+		Fixed(float const x);
 		Fixed(Fixed const & obj);
+
+		// Destructors
 		~Fixed(void);
 
-		int		getRawBits(void) const;
-		void	setRawBits(int const raw);
-
+		// Overloaded operators
 		Fixed	& operator= (Fixed const & rhs);
+
+		// Member functions
+		int		getRawbits(void) const;
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
 	private:
 		int					_RawBits;
-		static const int	_NbBits = 8;
+		static int const	_NbBits = 8;
 };
+
+std::ostream	& operator<< (std::ostream & ofs, Fixed const & fixed);
 
 #endif
