64e7fcfa5309d63f9da61d11e7cb61346bdd6e2d
mbucci <mbucci@student.s19.be>
2022-04-04 15:08:09 +0200

    ex00 perfected

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 13:25:35 by mbucci            #+#    #+#             */
-/*   Updated: 2022/03/16 13:46:42 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/04/04 15:06:58 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ int	main(void)
 	std::cout << "simple zombie:" << std::endl;
 	Zombie	z1("Albert");
 	z1.announce();
+
 	std::cout << std::endl << "zoombie sur le tas:" << std::endl;
 	Zombie	*z2 = newZombie("Gilbert");
 	z2->announce();
+
 	std::cout << std::endl << "randomChump:" << std::endl;
 	randomChump("Robert");
+
 	delete (z2);
 	return (0);
 }
